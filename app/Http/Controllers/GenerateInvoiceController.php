49804927e1d38329ffce21a66addbba575afd6b4
khouloud HADDAD AMAMOU <khouloud.haddad@gmail.com>
2023-07-05 21:26:37 +0000

    Add Storage facade dependency

--- app/Http/Controllers/GenerateInvoiceController.php
+++ app/Http/Controllers/GenerateInvoiceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
  use Barryvdh\DomPDF\Facade\Pdf;
+ use Illuminate\Support\Facades\Storage;
  use App\Models\Invoice;
 
 class GenerateInvoiceController extends Controller
